976832fa3de292acfa9a5344ac61f71b881ed735
fares13240 <faresz1324@gmail.com>
2025-03-01 11:04:59 +0100

    Init project

---
+++ src/test/java/com/projectarchi/ProjectArchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectarchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectArchiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
